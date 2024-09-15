a89e706545c4d371ec3c6f4695413ba59269a7e3
Jay Gandhi <gandhijay126@gmail.com>
2024-09-15 10:29:59 +0530

    Carousel Admin

---
+++ lib/screens/admin/carousel/Add_carousel_screen.dart
@@ -0,0 +1,10 @@
+class AddCarouselScreen extends StatelessWidget {
+  const AddCarouselScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scafold(
+      body:Text("Add Carousel Screen");
+    );
+  }
+}
